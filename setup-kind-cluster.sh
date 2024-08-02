8645901aa65e08be56c67a67d6db7651fd93ae75
Jorge Puerta <jorge.puerta00@gmail.com>
2024-08-02 15:32:26 +0200

    chore: updated dockerfile

--- setup-kind-cluster.sh
+++ setup-kind-cluster.sh
@@ -14,20 +14,34 @@ then
     exit 1
 fi
 
-# Create a kind cluster
-kind create cluster --name otlp-demo-cluster
+# Check if the kind cluster already exists
+if kind get clusters | grep -q "otlp-demo-cluster"; then
+    echo "Kind cluster 'otlp-demo-cluster' already exists. Skipping creation."
+else
+    # Create a kind cluster
+    kind create cluster --name otlp-demo-cluster
 
-# Verify if the kind cluster was created
-if ! kind get clusters | grep -q "otlp-demo-cluster"; then
-    echo "Kind cluster was not created successfully. Exiting."
-    exit 1
+    # Verify if the kind cluster was created successfully
+    if ! kind get clusters | grep -q "otlp-demo-cluster"; then
+        echo "Kind cluster was not created successfully. Exiting."
+        exit 1
+    fi
 fi
 
 # Build the Docker image
 docker build -t my-node-app .
 
+# Tag the Docker image
+docker tag my-node-app:latest jorgepuerta00/my-node-app:latest
+
+# Push the Docker image to Docker Hub
+docker push jorgepuerta00/my-node-app:latest
+
 # Load the Docker image into kind cluster
-kind load docker-image my-node-app --name otlp-demo-cluster
+kind load docker-image jorgepuerta00/my-node-app:latest --name otlp-demo-cluster
+
+# Update the deployment with the new image
+kubectl set image deployment/otel-dice-deployment dice-otl=jorgepuerta00/my-node-app:latest
 
 # Apply Kubernetes manifests
 kubectl apply -f k8s/otlp-configmap.yaml
